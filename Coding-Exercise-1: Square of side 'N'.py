cac82209a124f7532e9368ad062a3745b14de456
Deep Usdadiya <109851907+deepusdadiya@users.noreply.github.com>
2025-03-27 14:50:39 +0530

    Update Coding-Exercise-1: Square of side 'N'.py

--- Coding-Exercise-1: Square of side 'N'.py
+++ Coding-Exercise-1: Square of side 'N'.py
@@ -1,21 +1,21 @@
-Square of side 'N'
-Problem Description: You are given an integer n. Your task is to return a square pattern of size n x n made up of the character '*', represented as a list of strings.
+# Square of side 'N'
+# Problem Description: You are given an integer n. Your task is to return a square pattern of size n x n 
+# made up of the character '*', represented as a list of strings.
 
-Input Parameters:
-n (int): The size of the square (number of rows and columns).
+# Input Parameters:
+# n (int): The size of the square (number of rows and columns).
 
-Output:
-A list of strings where each string is a row of n characters.
+# Output:
+# A list of strings where each string is a row of n characters.
 
-Example:
-Input: 3
-Output: ['***', '***', '***']
+# Example:
+# Input: 3
+# Output: ['***', '***', '***']
  
-Input: 5
-Output: ['*****', '*****', '*****', '*****', '*****']
+# Input: 5
+# Output: ['*****', '*****', '*****', '*****', '*****']
 
-Solution:
-
+# Solution:
 def generate_square(n):
     """
     Function to return a square pattern of '*' of side n as a list of strings.
