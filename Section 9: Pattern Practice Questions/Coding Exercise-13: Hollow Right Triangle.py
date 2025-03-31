838334bbf57cffaf0d8b33fa85d258a7dad68970
Deep Usdadiya <109851907+deepusdadiya@users.noreply.github.com>
2025-03-31 12:28:20 +0530

    Create Coding Exercise-13: Hollow Right Triangle.py

---
+++ Section 9: Pattern Practice Questions/Coding Exercise-13: Hollow Right Triangle.py
@@ -0,0 +1,46 @@
+# Hollow Right Triangle
+# Problem Description:
+
+# You are given an integer n. Your task is to return a hollow right-angled triangle pattern of '*', 
+# where the first and last rows contain stars, while the inner rows contain a star at the beginning and end, 
+# with spaces in between. The triangle should be right-aligned.
+
+# Input:
+# A single integer n, where 1 <= n <= 100.
+
+# Output:
+# A list of strings where each string represents a row in the hollow right-angled triangle.
+
+# Example:
+# Input: 4
+# Output: ['*', '**', '* *', '****']
+ 
+# Input: 5
+# Output: ['*', '**', '* *', '*  *', '*****']
+
+# Solution:
+
+def generate_hollow_right_angled_triangle(n):
+    """
+    Function to return a hollow right-angled triangle of '*' of side n as a list of strings.
+    
+    Parameters:
+    n (int): The height of the triangle.
+    
+    Returns:
+    list: A list of strings where each string represents a row of the triangle.
+    """
+    # Your code here
+    string=''
+    string_list=[]
+    for i in range(n):
+        if i == 0 or i == 1 or i == (n-1):
+            string = '*'*(i+1)
+            string_list.append(string)
+        else:
+            string= '*' + ' '*(i-1) + '*'
+            string_list.append(string)
+    print(string_list)
+    return string_list
+    
+generate_hollow_right_angled_triangle(5)
