f12c2b89ec49f9953accf928fc92fe6a6825c3ff
Deep Usdadiya <109851907+deepusdadiya@users.noreply.github.com>
2025-03-31 13:22:30 +0530

    Create Coding Exercise-16: Celsius to Fahrenheit.py

---
+++ Section 10: Function Practice Questions/Coding Exercise-16: Celsius to Fahrenheit.py
@@ -0,0 +1,41 @@
+# Celsius to Fahrenheit
+# Problem Description:
+
+# You are given a temperature in Celsius. Your task is to convert it to Fahrenheit and return the result.
+
+# Formula:
+# To convert Celsius to Fahrenheit, use the formula:
+# F = (9/5 * C) + 32
+# Where F is the temperature in Fahrenheit and C is the temperature in Celsius.
+
+# Input:
+# A floating-point number C representing the temperature in Celsius.
+
+# Output:
+# A floating-point number representing the temperature in Fahrenheit.
+
+# Example:
+# Input: C = 25
+# Output: 77.0
+ 
+# Input: C = 0
+# Output: 32.0
+
+# Solution:
+
+def celsius_to_fahrenheit(C):
+    """
+    Function to convert temperature from Celsius to Fahrenheit.
+    
+    Parameters:
+    C (float): The temperature in Celsius.
+    
+    Returns:
+    float: The temperature in Fahrenheit.
+    """
+    # Your code here
+    F = (9/5 * C) + 32
+    print(F)
+    return F
+    
+celsius_to_fahrenheit(25)
